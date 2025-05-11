ab19f4f110a47185c2f32e27a89164fe38d45fea
Nicolas <nicosan12@hotmail.com>
2025-05-10 22:16:02 -0500

    first commit

---
+++ src/test/java/com/auth2/app/Auth2TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth2.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth2TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
